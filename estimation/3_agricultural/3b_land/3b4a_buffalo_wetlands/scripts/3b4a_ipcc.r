b19d0e1de7f7f367a6e1fc3693acf70aa5a2d347
Sergio Ibarra Espinosa <zergioibarra@gmail.com>
2026-04-28 17:23:59 -0400

    3 done

---
+++ estimation/3_agricultural/3b_land/3b4a_buffalo_wetlands/scripts/3b4a_ipcc.r
@@ -0,0 +1,256 @@
+# IPCC
+# 1: Energy
+# 1.A Fuel Combustion Activities
+# 1.A.3 - IPCC Transport
+# 1.A.3.a - IPCC Civil Aviation\n"
+# 1.A.3.a - IPCC Civil Aviation\n1.A.3.a.i - International Aviation (International Bunkers)\n1.A.3.a.ii - Domestic Aviation\n"
+# 1.A.3.a - IPCC Civil Aviation\n1.A.3.a.ii - Domestic Aviation\n
+# 1.A.3.a.i - IPCC International Aviation (International Bunkers)\n
+# 1.A.3.a.ii - IPCC Domestic Aviation\n
+# 1.A.3.a.i.(i) - EMEP International aviation (LTO)
+# 1.A.3.a.ii.(i) - EMEP "Civil aviation (domestic, LTO)
+# 1.A.3.b - Road Transportation\n"
+# 1.A.3.b - Road Transportation\n1.A.3.b.ii - Light-duty trucks\n1.A.3.b.iii - Heavy-duty trucks and buses\n"
+# 1.A.3.b - Road Transportation\n1.A.3.b.i - Cars\n"
+# "Road transport, passenger cars"
+# "1.A.3.b.i - Cars\n"
+# "1.A.3.b.i - Cars\n
+# 1.A.3.b.ii - Light-duty trucks\n"
+# "1.A.3.b.i.1 - Passenger cars with 3-way catalysts\n"
+# "1.A.3.b.ii - Light-duty trucks\n"
+# "1.A.3.b.iii - Heavy-duty trucks and buses\n"
+# "1.A.3.b.iv - Motorcycles\n"
+# "1.A.3.b.v - Road transport, gasoline evaporation"
+# "1.A.3.b.vi - "Road transport, automobile tyre and brake wear"
+# "1.A.3.c - "Railways"             "1.A.3.c - Railways\n"
+# "1.A.3.d - Water-borne Navigation\n"
+# 1.A.3.d.i - International water-borne navigation (International bunkers)\n"
+# 1.A.3.d.ii - "National navigation"
+# 1.A.3.e.ii - Off-road
+# "1.A.4 - Other Sectors\n"
+# "1.A.4.a - Commercial/Institutional\n"
+# "1.A.4.a.i - Commercial / institutional: stationary
+# "1.A.4.a.ii -"Commercial / institutional: mobile"
+# "1.A.5.a - "Other, stationary (including military)"
+# "1.A.5.b"
+
+# "1.B.1.a - Coal mining and handling\n"
+# "1.B.1.b "Fugitive emissions from solid fuels - Solid fuel transformation"
+
+# "1.B.2.a - Oil\n"
+# "1.B.2.b - Natural Gas\n"
+# 1.B.3 - Other emissions from Energy Production\n"
+# "2.A.1 - Cement production\n"
+# "2.A.2 - Lime production\n"
+# "2.A.3 - Glass Production\n"
+# "2.A.4 - Other Process Uses of Carbonates\n"
+# "2.A.4.a - Ceramics\n"
+# "2.A.4.b - Other Uses of Soda Ash\n"
+# "2.A.4.c - Non Metallurgical Magnesia Production\n"
+# "2.B.1 - Ammonia Production\n"
+# "2.B.2 - Nitric Acid Production\n"
+# "2.B.3 - Adipic Acid Production\n"
+# "2.B.4 - Caprolactam, Glyoxal and Glyoxylic Acid Production\n"
+# "2.B.5 - Carbide Production\n"
+# "2.B.6 - Titanium Dioxide Production\n"
+# "2.B.7 - Soda Ash Production\n"
+# "2.B.8.a - Methanol\n"
+# "2.B.8.b - Ethylene\n"
+# "2.B.8.c - Ethylene Dichloride and Vinyl Chloride Monomer\n"
+# "2.B.8.d - Ethylene Oxide\n"
+# "2.B.8.e - Acrylonitrile\n"
+# "2.B.8.f - Carbon Black\n"
+# "2.B.9.a - By-product emissions hfc\n"
+# "2.B.9.b - Fugitive Emissions hfc\n"
+# "2.B.10.a - Chemical industry: Other"
+
+# "2.C.1 - Iron and Steel Production\n"
+# "2.C.2 - Ferroalloys Production\n"
+# "2.C.3 - Aluminium production\n"
+# "2.C.4 - Magnesium production\n"
+# "2.C.5 - Lead Production\n"
+# "2.C.6 - Zinc Production\n"
+# "2.C.7.a - Copper production"
+# "2.C.7.b - Nickel production"
+# "2.C.7.c - "Other metal production"
+# "2.C.7.d - Storage, handling and transport of metal products
+# "2.D.1 - Lubricant Use\n"
+# "2.E - Electronics Industry\n2.E.2 - TFT Flat Panel Display\n"
+# "2.E.1 - Integrated Circuit or Semiconductor\n"
+# "2.F.1 - Refrigeration and Air Conditioning\n"
+# "2.F.1.a - Refrigeration and Stationary Air Conditioning\n"
+# "2.F.1.b - Mobile Air Conditioning\n"
+# "2.F.2 - Foam Blowing Agents\n"
+# "2.F.3 - Fire Protection\n"
+# "2.F.4 - Aerosols\n"
+# "2.F.5 - Solvents\n"
+# "2.F.6 - Other Applications (please specify)\n"
+# 2.G - Preservation of wood"
+# "2.G.1 - Electrical Equipment\n"
+# "2.G.1.a - Manufacture of Electrical Equipment\n"
+# "2.G.1.b - Use of Electrical Equipment\n"
+# "2.G.1.c - Disposal of Electrical Equipment\n"
+# "2.G.2.a - Military Applications\n"
+# "2.G.2.b - Accelerators\n"
+# "2.G.3.a - Medical Applications\n"
+# "2.G.3.b - Propellant for pressure and aerosol products\n"
+# "2.H.1 -"Pulp and paper industry"
+# "2.I - Wood processing
+# 2K - "Consumption of POPs and HMs"
+# "3 - Agriculture, Forestry, and Other Land Use\n"
+# "3.A - Livestock\n3"
+# "3.A.1 - Enteric Fermentation\n"
+# "3.A.1.a - Cattle\n"
+# "3.A.1.a.i - Dairy Cows\n"
+# "3.A.1.a.ii - Other Cattle\n"
+# "3.A.1.b - Buffalo\n"
+# "3.A.1.c - Sheep\n"
+# "3.A.1.d - Goats\n"
+# "3.A.1.e - Camels\n"
+# "3.A.1.f - Horses\n"
+# "3.A.1.g - Mules and Asses\n"
+# "3.A.1.h - Swine\n"
+# "3.A.1.j - Other (please specify)\n"
+# "3.A.2 - Manure Management\n"
+# "3.A.2.a - Cattle\n"
+# "3.A.2.a.i - Dairy cows\n"
+# "3.A.2.a.ii - Other cattle\n"
+# "3.A.2.b - Buffalo\n"
+# "3.A.2.c - Sheep\n"
+# "3.A.2.d - Goats\n"
+# "3.A.2.e - Camels\n"
+# "3.A.2.f - Horses\n"
+# "3.A.2.g - Mules and Asses\n"
+# "3.A.2.h - Swine\n"
+# "3.A.2.i - Poultry\n"
+# "3.A.2.j - Other (please specify)\n"
+
+# "3.B - Land\n"
+
+# "3.B.1 - Cattle\n" EMEP
+# "3.B.1 - Forest land\n" IPCC
+
+# "3.B.1.a - Dairy cattle\n" EMEP
+# "3.B.1.a - Forest land Remaining Forest land\n" IPCC
+
+# "3.B.1.b - Non-dairy cattle \n" EMEP
+# "3.B.1.b - Land Converted to Forest land\n" IPCC
+
+# "3.B.1.b.i - Cropland converted to Forest Land"     
+# "3.B.1.b.ii - Grassland converted to Forest Land"
+# "3.B.1.b.iii - Wetlands converted to Forest Land\n"
+
+# "Sheep" EMEP
+# "3.B.2 - Cropland\n"  #IPCC
+# "3.B.2.a - Cropland Remaining Cropland\n" 
+# "3.B.2.b - Land Converted to Cropland\n"
+# "3.B.2.b.i - Forest Land converted to Cropland\n"
+
+# "3.B.3 - Swine" #EMEP
+# "3.B.3 - Grassland\n" #IPCC
+# "3.B.3.a - Grassland Remaining Grassland\n"
+# "3.B.3.b - Land Converted to Grassland\n"
+
+# "3.B.4 - Wetlands\n"
+# "3.B.4.a - Buffalo"
+# "3.B.4.a - Wetlands Remaining Wetlands\n"  
+
+library(data.table)
+library(pega)
+
+# database
+db <- ef(returnfdb = T)
+
+db[,
+  grep("3.B.4", code, value = T)
+] |>
+  unique() -> ll
+
+ll[order(ll)]
+
+# database final
+db[
+  code == "3.B.4.a"
+] -> dbf
+
+unique(dbf$pol)
+
+# category
+dbf[, unique(category)]
+
+# type
+dbf[, unique(region), by = pol]
+
+db[
+  code == "3.B.4.a",
+  unique(tech),
+  by = source
+][source == "EMEP"]
+
+db[
+  code == "3.B.4.a",
+  unique(unit),
+  by = source
+][source == "EMEP"]$V1
+
+db[
+  code == "3.B.4.a",
+  unique(tech),
+  by = source
+][source == "IPCC"]
+
+db[
+  code == "3.B.4.a",
+  unique(unit),
+  by = source
+][source == "IPCC"]
+
+db[
+  code == "3.B.4.a" 
+][unit == "g C/ha"][1] -> db_ef
+
+db_ef[, pol := gsub(" ", "", pol)]
+
+db_ef[, unique(source)]
+
+db_ef[, unique(tech2)]
+
+db_ef[, .N, by = .(pol, source)]
+
+activity <- data.table(
+  id = 1,
+  lat = -23,
+  lon = -46,
+  alt = 10,
+  code = "3.B.4.a",
+  activity = rnorm(n = 12, mean = 500, sd = 100),
+  unit = "ha",
+  date = seq.Date(as.Date("2020-01-01"), length.out = 12, by = "month"),
+  region = "HERE"
+)
+
+rbindlist(lapply(1:nrow(activity), function(i) {
+  df <- db_ef
+  df$id <- activity$id[i]
+  df$lat <- activity$lat[i]
+  df$lon <- activity$lon[i]
+  df$alt <- activity$alt[i]
+
+  df$activity <- activity$activity[i]
+  df$unit_activity <- activity$unit[i]
+  df$date_activity <- activity$date[i]
+  df$region <- activity$region[i]
+  df
+})) -> dt
+
+dt[, emissions := ef * activity] # need to change unit of ef from kg to g
+# BC is % of PM2.5
+# dt[pol == "PM2.5"]
+# dt[pol == "BC"]
+# dt[pol == "BC", emissions := ef  * dt[pol == "PM2.5"]$emissions]
+# dt[pol == "BC"]
+
+fwrite(
+  dt,
+  "estimation/3_agricultural/3b4/emissions/3b4a_ipcc.csv"
+)
